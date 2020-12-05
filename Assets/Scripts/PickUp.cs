495421fc642f5a3e0a6b53523254a45aaae8bec1
StaZarycki <stazarycki@gmail.com>
2020-12-05 23:19:33 +0100

    It's getting even better! (kind of)

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed;
+
+    private Vector2 movement = Vector2.down;
+
+    private float speedGainMultiplier;
+    private float animationTime;
+    private float speedGain;
+
+    private void Awake()
+    {
+        // Set initial values
+        speedGainMultiplier = 10;
+    }
+
+    public virtual void Start()
+    {
+        // Set starting animation time for tan function
+        animationTime = (Mathf.PI / 2) * 0.9f;
+    }
+
+    private void Update()
+    {
+        AnimateTan();
+        DrawDebugLine();
+    }
+
+    private void OnBecameInvisible()
+    {
+        Die();
+    }
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.Translate(movement * Time.deltaTime, Space.Self);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Activate pickup and destroy on collision with player
+        if (collision.CompareTag("Player"))
+        {
+            OnPlayerPickUp(collision);
+            Die();
+        }
+    }
+
+
+
+    private void AnimateTan()
+    {
+        // Make a tan of time function for animation and clamp it to maxSpeed
+        animationTime = Mathf.Min(animationTime + Time.time * 0.01f, Mathf.PI * 0.99f);
+        speedGain = Mathf.Tan((animationTime - Mathf.PI / 2)) * speedGainMultiplier;
+        movement = Vector2.ClampMagnitude(Vector2.down * speedGain, maxSpeed);
+    }
+
+    private void DrawDebugLine()
+    {
+        // Draw line from PowerUp to movement Vector
+        Debug.DrawLine(gameObject.transform.position, new Vector2(
+            gameObject.transform.position.x,
+            gameObject.transform.position.y) + movement, new Color(0.8f, 0.5f, 0.5f));
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public virtual void OnPlayerPickUp(Collider2D playerCollider)
+    {
+        Debug.Log(gameObject.name + ": picked up");
+    }
+}
--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -2,86 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUp : MonoBehaviour
+public class PowerUp : PickUp
 {
-
-    [SerializeField] private float maxSpeed;
     [SerializeField] private bool isBig;
 
-    private GameObject playerObject;
-    private Vector2 movement = Vector2.down;
-
-    private float speedGainMultiplier;
-    private float animationTime;
-    private float speedGain;
-
-    private void Awake()
+    public override void Start()
     {
-        // Set initial values
-        speedGainMultiplier = 10;
-        playerObject = GameObject.FindGameObjectWithTag("Player");
-    }
+        base.Start();
 
-    private void Start()
-    {
-        // Set starting animation time for tan function
-        animationTime = (Mathf.PI / 2) * 0.9f;
-    }
-
-    private void Update()
-    {
-        AnimateTan();
-        DrawDebugLine();
-    }
-
-    private void FixedUpdate()
-    {
-        gameObject.transform.Translate(movement * Time.deltaTime, Space.Self);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        // Give player more power on contact and die
-        if (collision.CompareTag("Player"))
+        // Make bigger if isBig (duh)
+        if (isBig)
         {
-            if (isBig)
-            {
-                playerObject.GetComponent<PlayerController>().AddMorePower(10);
-            }
-            else
-            {
-                playerObject.GetComponent<PlayerController>().AddMorePower(20);
-            }
-
-            Die();
+            gameObject.transform.localScale = new Vector2(1.5f, 1.5f);
         }
     }
 
-    private void OnBecameInvisible()
+    public override void OnPlayerPickUp(Collider2D playerCollider)
     {
-        Die();
-    }
+        base.OnPlayerPickUp(playerCollider);
 
-
-
-    private void AnimateTan()
-    {
-        // Make a tan of time function for animation and clamp it to maxSpeed
-        animationTime = Mathf.Min(animationTime + Time.time * 0.01f, Mathf.PI * 0.99f);
-        speedGain = Mathf.Tan((animationTime - Mathf.PI / 2)) * speedGainMultiplier;
-        movement = Vector2.ClampMagnitude(Vector2.down * speedGain, maxSpeed);
-    }
-    
-    private void DrawDebugLine()
-    {
-        // Draw line from PowerUp to movement Vector
-        Debug.DrawLine(gameObject.transform.position, new Vector2(
-            gameObject.transform.position.x,
-            gameObject.transform.position.y) + movement, new Color(0.8f, 0.5f, 0.5f));
-    }
-
-    private void Die()
-    {
-        Destroy(gameObject);
+        // Add power to player
+        playerCollider.gameObject.GetComponent<PlayerController>().AddMorePower(isBig ? (byte) 10 : (byte) 1);
     }
 }
